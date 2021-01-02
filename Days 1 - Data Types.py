9b12c50311dca0ee6e6c37c87869e041c1eca6d4
ekremekiz <ekremekz@gmail.com>
2021-01-02 21:30:43 +0300

    Day 1 Commit

---
+++ Days 1 - Data Types.py
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+j = int(input())
+k = float(input())
+l = str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+j)
+# Print the sum of the double variables on a new line.
+print(d+k)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+l)
