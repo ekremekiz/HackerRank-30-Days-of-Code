3599a848eb06be366df4fb067bb88463a9a6b530
ekremekiz <ekremekz@gmail.com>
2021-01-05 00:16:32 +0300

    Day 13 Commit

--- Day 13 - Abstract Classes.py
+++ Day 13 - Abstract Classes.py
@@ -5,11 +5,13 @@ class Book(object, metaclass=ABCMeta):
         self.author=author   
     @abstractmethod
     def display(): pass
+
 #Write MyBook class
 class MyBook(Book):
     def __init__(self,title,author,price):
         super().__init__(title,author)
         self.price=price
+    
     def display(self):
         print("Title: "+ self.title)
         print("Author: " + self.author)
