af4d90ca8d0cfe79e7d1830167aa05e0641b5f48
ekremekiz <ekremekz@gmail.com>
2021-01-06 23:57:53 +0300

    Day 15 Commit

---
+++ Day 15 - Linked List.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+    #Complete this method
+        new_node = Node(data)
+        new_node.next = head
+        head.next = new_node
+        
+        return new_node
+        
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head);
